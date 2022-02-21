c843172d2e5ad26a7b9027747822a938272bb46c
Badischemi <80965161+baduss@users.noreply.github.com>
2022-02-21 18:38:39 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,6 +7,6 @@ print("Spaming in 4 seconds ...")
 time.sleep(4)
 for i in range(amount):
     i+=1
-    keyboard.write(txt+" ")
+    keyboard.write(txt)
     keyboard.press_and_release('enter')
     time.sleep(delay)
